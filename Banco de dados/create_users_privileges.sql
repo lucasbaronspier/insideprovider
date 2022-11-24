995c879f362386b730ff4ecdccb4a483b496a561
lucasbaronspier <lucasbaronspier@gmail.com>
2022-11-24 20:09:20 -0300

    Atualizado create_users_privileges

--- Banco de dados/create_users_privileges.sql
+++ Banco de dados/create_users_privileges.sql
@@ -20,3 +20,14 @@ grant select on endereco, usuario, funcionario, atendimento, contrato, plano to 
 -- Atribuindo permissoes para Administrador
 grant select, delete, insert, update on endereco, usuario, funcionario, atendimento, contrato, fatura, plano to administrador;
 
+-- Criando acessos para os usuarios do sistema
+create role joao password '111' in role comercial;
+create role lucas password '222' in role suporte;
+create role jonas password '333' in role financeiro;
+create role wiliam password '444' in role administrador;
+
+-- Atribuindo permissão para logar com o usuario
+alter role joao LOGIN;
+alter role lucas LOGIN;
+alter role jonas LOGIN;
+alter role wiliam LOGIN;

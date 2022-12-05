eca04907ca60ebb29fcce16d9f4f52d6b353e08e
Willian Colombo <williancolombo511@gmail.com>
2022-12-04 22:27:10 -0300

    - Criação dos scripts para backup e restore - Criação do Dicionário de Dados 02_2022.pdf

---
+++ Banco de dados/Executável Backup.bat
@@ -0,0 +1,38 @@
+@ECHO off
+TITLE InsideProvider
+cls
+color a
+
+
+:MenuInicial
+cls
+ECHO.
+ECHO    ---------------------------------------
+ECHO    -- * MENU BACKUP INSIDEPROVIDER  * -- 
+ECHO    -- *   ESCOLHA UMA OPCAO ABAIXO    * --
+ECHO    ---------------------------------------
+ECHO.  
+ECHO    [1] BACKUP
+ECHO    [X] Sair
+ECHO.
+set /p MenuInicial=
+if '%MenuInicial%'=='1' goto MenuInicial_backup
+if '%MenuInicial%'=='x' goto sair_MenuInicial
+if not %MenuInicial%=="1,X" (
+ECHO    Opcao [%MenuInicial%] invalida.
+ECHO    Verifique o menu acima.
+
+goto MenuInicial
+)
+
+:MenuInicial_backup
+MKDIR C:\Backup_InsideProvider
+cd C:\Backup_InsideProvider
+ECHO %date:~6,4%.%date:~3,2%.%date:~0,2% - %time:~0,2%:%time:~-8,2%: BACKUP iniciado >> sgbd.log
+ECHO Em execucao...
+"c:\Program Files\PostgreSQL\14\bin\pg_dump.exe" -Fc -U postgres -d inside_provider_db -C -Fp -f  backup.bak >> sgbd.log
+ren "C:\Backup_InsideProvider\backup.bak" "Backup_insideprovider_%date:~6,4%.%date:~3,2%.%date:~0,2%_%time:~0,2%%time:~-8,2%.bak" >> sgbd.log
+
+ECHO %date:~6,4%.%date:~3,2%.%date:~0,2% - %time:~0,2%:%time:~-8,2%: BACKUP concluido >> sgbd.log
+goto MenuInicial
+
